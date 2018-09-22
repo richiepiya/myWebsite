ca722d8acef955c4ba6fdac8a84c8be920dc5e71
masterpiya <rpiyasirisilp@hotmail.com>
2018-09-22 14:44:25 -0400

    Initial Commit

---
+++ src/Components/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">
+          LOGO
+        </div>
+
+        
+      </header>
+    );
+  }
+}
+
+export default Header;
