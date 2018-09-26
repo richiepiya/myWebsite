3e340c8afaf8d2afb8c2bea82bf7255eb65c6057
Richie <rpiyasirisilp@gmail.com>
2018-09-25 23:09:54 -0400

    added content to work

--- src/Components/Pages/aboutPage.js
+++ src/Components/Pages/aboutPage.js
@@ -13,8 +13,11 @@ class AboutPage extends Component {
 
         <div className="section-info" onScroll={fadeScroll}>
           <div id="aboutContainer">
-            <h2 className="fade-scroll" id="aboutTitle">Hello! I’m a developer and designer from Toronto living in Montreal.</h2>
-            <br></br>
+            <div id="subjectHeader">Hello! My name is Richie.</div>
+            <div className="fade-scroll" id="aboutTitle"><strong>I’m a developer and designer from Toronto living in Montreal.</strong></div>
+            <hr id="subjectLineAbout"></hr>
+
+            <br />
             <p id="aboutContent">I’m currently in my last semester of Computer Engineering at McGill University. I’ve previously interned at RBC’s Amplify program and at Pratt & Whitney Canada where I built exciting webapps for internal and external clients. I love creating awesome interfaces and working on personal projects to solve real life problems. In my free time, I enjoy playing the violin and the piano, travelling the world, and finding great deals on eBay. If you’re working on a cool application and need a developer, please feel free to contact me!</p>
           </div>
         </div>
@@ -24,7 +27,7 @@ class AboutPage extends Component {
   }
 }
 function fadeScroll() {
-  $(".fade-scroll").css("opacity", 1 - ($(".section-info").scrollTop())/100);
+  $(".fade-scroll").css("opacity", 1 - ($(".section-info").scrollTop())/120);
 }
 
 export default AboutPage;
--- src/Components/Pages/workPage.js
+++ src/Components/Pages/workPage.js
@@ -2,29 +2,50 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 
 class WorkPage extends Component {
-render() {
-  return (
-    <div className="container-fluid">
+  render() {
+    return (
+      <div className="container-fluid">
 
-      <div className="section-category">
-        <div className="workPageImg"></div>
-        <div id="workImg"></div>
-      </div>
-
-      <div className="section-info" onScroll={fadeScroll}>
-        <div id="workContainer">
-          <h2 className="fade-scroll" id="workTitle">Work work work</h2>
-          <br></br>
-          <p id="workContent">sample text.</p>
+        <div className="section-category">
+          <div className="workPageImg"></div>
+          <div id="workImg"></div>
         </div>
-      </div>
 
-    </div>
-  );
-}
-}
-function fadeScroll() {
-$(".fade-scroll").css("opacity", 1 - ($(".section-info").scrollTop())/100);
+        <div className="section-info">
+          <div id="workContainer">
+            <div id="subjectHeader">RBC Royal Bank - Toronto, ON</div>
+            <div id="workTitle"><strong>AMPLIFY DEVELOPER</strong></div>
+
+            <hr id="subjectLineWork"></hr>
+            <br />
+
+            <div id="workContent">
+              <div id="subjectHeader"><strong>THE TASK</strong></div>
+              <br />
+              <div id="workContent">The Amplify program is RBC's summer innovation program that lets students apply their skills to build a product that could disrupt how RBC operates. My team and I were assigned a business challenge from RBC Ventures. Our goal? Help Canadians understand what it's like to live in a neighborhood without actually moving there.
+                <br /><br />
+                My tasks
+                <ul>
+                  <li>Developed responsive features that integrated data from the City of Toronto with the Google Maps API</li>
+                  <li>Collaborated with designer to create website UI/UX</li>
+                  <li>Conducted market research to identify and analyze user needs</li>
+                </ul>
+              </div>
+
+              <div id="subjectHeader"><strong>THE JOURNEY</strong></div>
+              <br />
+              <div id="workContent">To better understand the problem, my team and I conducted market research with real Canadians. Our goal was to find the root of their problems when searching for areas to live in. From this, we were able to identify user needs and the findings helped us ideate possible solutions. Throughout the summer, we kept in close contact with the RBC Ventures team for support and guidance and this ultimately helped us create our solution.</div>
+
+              <div id="subjectHeader"><strong>THE SOLUTION</strong></div>
+              <br />
+              <div id="workContent">The solution we came up with was Relevent. A hyperlocal community event finder, personalized to each user's lifestyle. By finding events in an area, Relevent helps users explore the community and understand what life could be like there.</div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default WorkPage;
